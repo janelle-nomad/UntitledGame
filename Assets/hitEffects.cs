c3e52ec30bf55d7beb4f5abf5c0e24462c9e47e8
janelle-nomad <janelle.saige@gmail.com>
2016-10-08 22:42:34 -0400

    Add project files.

---
+++ Assets/hitEffects.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+     /*
+        Janellle Heron
+        Unitiled 2D Platformer
+        Version 0.01
+     
+     */
+public class hitEffects : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
